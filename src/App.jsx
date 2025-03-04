57f7bd5851b9171043c7fceb7a18fe82f28bb383
Daniel Ermakov <danielermakov2012@gmail.com>
2025-03-04 17:56:58 +0200

    0.1.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import helldiverLogo from "./assets/helldivers_salute.jpg";
 import "./App.css";
 
 function App() {
@@ -9,21 +8,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={helldiverLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Edgar Temkin's Portfolio</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">בובי ז'וז'י</p>
     </>
